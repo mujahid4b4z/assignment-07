b7e2baa74d9915ad72056725d316d0e07238a70a
mujahid4bzb <mujahid4bzb@gmail.com>
2022-06-22 12:58:07 +0530

    files added

---
+++ script.js
@@ -0,0 +1,70 @@
+alert("Press OK to Enter");
+
+//VARIABLES
+var y;
+console.log(y);
+
+var x=10;
+console.log(x);
+
+/*PRIMITIVE DATA TYPE
+They are 6 primitive data types:
+string
+number
+boolean
+null
+undefined
+symbol()*/
+
+//string
+var a = "Mujahid"
+console.log(typeof a);
+
+var b = "25";
+console.log(typeof b);
+
+//Number
+var age = 45;
+console.log(typeof age);
+
+var phoneno =7545451456144;
+console.log(typeof phoneno);
+
+//Boolean
+var x = false;
+console.log(typeof x);
+
+var x = true;
+console.log(typeof x);
+
+//null
+var absentes = null;
+console.log(typeof absentes);
+
+//Undefined
+var statement;
+console.log(typeof statement);
+
+var sentence = undefined;
+console.log(typeof sentence);
+
+/*Symbol
+var vol = symbol();
+console.log(typeof vol);*/
+
+//Coercion
+var type1 = "elephant";
+var type2 = "horse";
+var sum = type1 + type2;
+console.log(sum);
+
+var value1 = "15";
+var value2 = "21";
+var sum = value1 + value2;
+console.log(sum);
+
+//String Concatenation
+var msg1 = "They are ";
+var numhotels = 18;
+var msg2 = " Hotels in Town"
+console.log(msg1 + numhotels + msg2);
